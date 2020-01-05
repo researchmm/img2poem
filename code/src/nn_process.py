ab787c445c85f4f4c9d51140a09b67e28414b1cc
wkcn <wkcn@live.cn>
2020-01-05 09:52:47 +0800

    fix pickle bug and remove caffe_io.py

--- code/src/nn_process.py
+++ code/src/nn_process.py
@@ -1,21 +1,21 @@
 import multiprocessing
 import importlib
 
-def create(module_name):
-    def pipe_process(pipe):
-        lib = importlib.import_module(module_name)
-        func = getattr(lib, module_name)
-        pipe.send('Okay :-)')
-        while 1:
-            data = pipe.recv()
-            try:
-                poem = func(data)
-                pipe.send(poem)
-            except Exception as e:
-                pipe.send(e)
+def pipe_process(pipe, module_name):
+    lib = importlib.import_module(module_name)
+    func = getattr(lib, module_name)
+    pipe.send('Okay :-)')
+    while 1:
+        data = pipe.recv()
+        try:
+            poem = func(data)
+            pipe.send(poem)
+        except Exception as e:
+            pipe.send(e)
 
+def create(module_name):
     pipes = multiprocessing.Pipe() # W/R
-    proc = multiprocessing.Process(target=pipe_process, args=(pipes[1],))
+    proc = multiprocessing.Process(target=pipe_process, args=(pipes[1], module_name))
     proc.start()
     # wait to start
     print (pipes[0].recv())
